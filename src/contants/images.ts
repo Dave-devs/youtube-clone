d20a4ac0d92c72704d917c56644466f390d1a37f
Dave-devs <Odetundeoreoluwadavid@gmail.com>
2024-10-06 03:05:12 +0100

    Removed thumbnail import and reference from images object

--- src/contants/images.ts
+++ src/contants/images.ts
@@ -1,6 +1,5 @@
 import logo from "../assets/logo.png";
 import logoWithText from "../assets/logo-with-text.png";
-import thumbnail from "../assets/thumbnail.jpg";
 import profile from "../assets/profile.png";
 import trump from "../assets/trump.jpg";
 import chris from "../assets/chris.jpg";
@@ -11,7 +10,6 @@ import burna from "../assets/burna.png";
 const images = {
   logo,
   logoWithText,
-  thumbnail,
   profileImg: profile,
   trump,
   chris,
